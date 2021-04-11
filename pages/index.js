9bd708b2f14e87dd13f516ae5477192cccd33d4e
Md. Asaduzzaman <md.asad.mony@gmail.com>
2021-04-11 23:15:00 +0600

    home page edited

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,15 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div>
       <Head>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>First Next App</title>
         <link rel="icon" href="/favicon.ico" />
-
+        <meta name="keywords" content="next, js, server side rendering, node, react"></meta>
+        <meta name="author" content="Md. Asaduzzaman, md.asad.mony@gmail.com, +8801753007145"></meta>
       </Head>
 
       <main>
